942b935380a1962de45f8b3adb5935b6f77a5eb9
Anurag <anuragtech24.7@gmail.com>
2020-07-03 12:47:19 +0530

    Blocking code - blocking the event loop

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
-
 const app = express();
 
+/**
+ * Blocking the event loop
+ *
+ */
+
+// This code gets executed inside the event loop, not in thread pool nor offloaded to OS
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
+}
+
 app.get("/", (req, res) => {
+  doWork(5000);
   res.send("Hi there!");
 });
 
