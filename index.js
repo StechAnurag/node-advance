ee8fa4d3a55b5496aa12b176b3f3dd2b89998a61
Anurag <anuragtech24.7@gmail.com>
2020-07-15 08:50:32 +0530

    implemeting cluster mode

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const express = require("express");
-const app = express();
+const cluster = require('cluster');
 
-/**
- * Blocking the event loop
- *
- */
+// console.log(cluster.isMaster);
 
-// This code gets executed inside the event loop, not in thread pool nor offloaded to OS
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but in child mode
+  cluster.fork();
+  //   cluster.fork();
+  //   cluster.fork();
+  //   cluster.fork();
+} else {
+  // I m a child, I am going to act like a server and do nothing else
+  const express = require('express');
+  const app = express();
+
+  /**
+   * Blocking the event loop
+   *
+   */
+
+  // This code gets executed inside the event loop, not in thread pool nor offloaded to OS
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there!');
+  });
+
+  app.get('/fast', (req, res) => {
+    res.send('This route was fast!');
+  });
+
+  app.listen(3000);
 }
-
-app.get("/", (req, res) => {
-  doWork(5000);
-  res.send("Hi there!");
-});
-
-app.listen(3000);
