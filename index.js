0b4f4f5d77149d12cd606e6af9bcf810c119ed59
Anurag <anuragtech24.7@gmail.com>
2020-07-17 09:09:21 +0530

    refactoring lec 27

--- index.js
+++ index.js
@@ -20,13 +20,16 @@ if (cluster.isMaster) {
    */
 
   // This code gets executed inside the event loop, not in thread pool nor offloaded to OS
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
+  /**
+   *  [Refactoring after lecture 27]
+   */
+  // function doWork(duration) {
+  //   const start = Date.now();
+  //   while (Date.now() - start < duration) {}
+  // }
 
   app.get('/', (req, res) => {
-    doWork(5000);
+    // doWork(5000);
     res.send('Hi there!');
   });
 
