461a74a98248bdd405d66753193274aded3bc9be
Anurag <anuragtech24.7@gmail.com>
2020-09-21 16:14:23 +0530

    refactoring cache routes

---
+++ services/cache.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function () {
+  console.log("I am about to run a query");
+
+  return exec.apply(this, arguments);
+};
