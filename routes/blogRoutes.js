e9029e12c84e62d15fd6075ab512a051d162ec09
Anurag <anuragtech24.7@gmail.com>
2020-09-21 20:02:03 +0530

    complete caching solution

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,47 +1,35 @@
-const mongoose = require("mongoose");
-const chalk = require("chalk");
-const requireLogin = require("../middlewares/requireLogin");
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+const requireLogin = require('../middlewares/requireLogin');
+// const { clearHash } = require('../services/cache');
+const clearHash = require('../middlewares/clearCache');
 
-const Blog = mongoose.model("Blog");
+const Blog = mongoose.model('Blog');
 
-module.exports = (app) => {
-  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
+module.exports = app => {
+  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id,
+      _id: req.params.id
     });
 
     res.send(blog);
   });
 
-  app.get("/api/blogs", requireLogin, async (req, res) => {
-    const redis = require("redis");
-    const redisUrl = "redis://127.0.0.1:6379";
-    const client = redis.createClient(redisUrl);
-
-    const { promisify } = require("util");
-    client.get = promisify(client.get);
-
-    const cachedBlogs = await client.get(req.user.id);
-    if (cachedBlogs) {
-      console.log(chalk.bold.bgMagenta("Serving from cache.."));
-      return res.send(JSON.parse(cachedBlogs));
-    }
-
-    const blogs = await Blog.find({ _user: req.user.id });
-    console.log(chalk.bold.bgBlue("Serving from DB.."));
+  app.get('/api/blogs', requireLogin, clearHash, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
     res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post("/api/blogs", requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id,
+      _user: req.user.id
     });
 
     try {
@@ -50,5 +38,8 @@ module.exports = (app) => {
     } catch (err) {
       res.send(400, err);
     }
+
+    // clear the cache on new post creation
+    // clearHash(req.user.id);
   });
 };
--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,51 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const redis = require('redis');
+const { promisify } = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+
+// client.get = promisify(client.get);
+client.hget = promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-  console.log("I am about to run a query");
+mongoose.Query.prototype.cache = function (options = {}) {
+  this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+  return this;
+};
 
-  return exec.apply(this, arguments);
+mongoose.Query.prototype.exec = async function () {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
+
+  const key = JSON.stringify(
+    Object.assign({}, this.getFilter(), {
+      collection: this.mongooseCollection.name
+    })
+  );
+
+  //   const cachedValue = await client.get(key);
+  const cachedValue = await client.hget(this.hashKey, key);
+
+  if (cachedValue) {
+    const doc = JSON.parse(cachedValue);
+    // console.log('serving from cache');
+    return Array.isArray(doc) ? doc.map(d => new this.model(d)) : new this.model(doc);
+  }
+  //  return exec.apply(this, arguments);
+
+  // otherwise, issue the query and store the result into redis
+  const result = await exec.apply(this, arguments);
+  //   client.set(key, JSON.stringify(result));
+  client.hset(this.hashKey, key, JSON.stringify(result));
+  return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  }
 };
